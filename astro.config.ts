83c34f5e13550b5c781d5cc593c45da3a23d1880
Rolling Peaches <marinalo.lm644@gmail.com>
2025-10-07 12:43:46 +0100

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+
+export default defineConfig({
+  // GitHub Deployment
+  
+
+  // used to generate images
+  site: 'https://marinalopez98.github.io'',
+  base: '/Maaz-Peaches',
+  trailingSlash: 'ignore',
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+  },
+});
